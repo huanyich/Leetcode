098175bab32fcf9ef5013f04d310291cc119c452
Yi-Chan Huang <84730018+huanyich@users.noreply.github.com>
2022-10-06 14:58:12 -0700

    Create 1. Two Sum.cs

---
+++ Blind75/1. Two Sum/1. Two Sum.cs
@@ -0,0 +1,21 @@
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var dict = new Dictionary<int, int>();
+        
+        for(int i = 0; i < nums.Length; i++)
+        {   
+            if (dict.ContainsKey(target - nums[i])) // checks if compliment is in dict
+            {
+                return new int[] { dict[target - nums[i]], i };
+            }
+            else if (!dict.ContainsKey(nums[i])) // handles duplicates in array
+            {
+                dict.Add(nums[i], i);
+            }
+        }
+        
+        return null;
+    }
+}
