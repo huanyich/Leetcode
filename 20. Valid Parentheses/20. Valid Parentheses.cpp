cee726a20b74b70d14b8900d733b366917990210
Yi-Chan Huang <84730018+huanyich@users.noreply.github.com>
2022-10-11 15:01:34 -0700

    Create 20. Valid Parentheses.cpp

---
+++ 20. Valid Parentheses/20. Valid Parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char c:s){
+            if(c=='{' || c=='(' || c=='[') st.push(c);
+            else{
+                if(st.empty()) return false;
+                if(c=='}'){
+                    if(st.top()=='{') st.pop();
+                    else return false;
+                }else if(c==')'){
+                    if(st.top()=='(') st.pop();
+                    else return false;
+                }else{
+                    if(st.top()=='[') st.pop();
+                    else return false;
+                }
+            }
+        }
+        return st.empty()?true:false;
+    }
+};
