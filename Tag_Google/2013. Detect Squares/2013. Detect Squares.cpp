5ae395522dc62eca96a24718b084365292a3929c
Yi-Chan Huang <84730018+huanyich@users.noreply.github.com>
2022-10-28 14:30:24 -0700

    Update 2013. Detect Squares.cpp

--- Tag_Google/2013. Detect Squares/2013. Detect Squares.cpp
+++ Tag_Google/2013. Detect Squares/2013. Detect Squares.cpp
@@ -1,4 +1,34 @@
-// Solution 1 Time Complexity: n^2
+//Solution 1: Time Complexity: O(n)
+class DetectSquares {
+    unordered_map<int, unordered_map<int,int>> graph;
+public:
+    DetectSquares() {
+        
+    }
+    
+    void add(vector<int> point) {
+        graph[point[0]][point[1]]++;
+    }
+    
+    int count(vector<int> point) {
+        int count = 0, x1 = point[0], y1 = point[1];
+        
+        for(auto &y2 : graph[x1]){
+            if(y2.first == y1)
+                continue;
+            int diff = abs(y1 - y2.first);
+            int xL = point[0] - diff;
+            int xR = point[0] + diff;
+            count += graph[xL][point[1]] * graph[xL][y2.first] * y2.second;
+            count += graph[xR][point[1]] * graph[xR][y2.first] * y2.second; 
+        }
+        return count;
+        
+    }
+};
+
+
+// Solution 2 Time Complexity: n^2
 class DetectSquares {
 public:
     unordered_map<int,vector<int>> Y;
